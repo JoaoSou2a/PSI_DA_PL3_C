36d48a28927fb91853bc6bd940ad28fe0f68f050
João Sousa <145674827+JoaoSou2a@users.noreply.github.com>
2024-06-21 18:49:52 +0100

    Add files via upload

---
+++ Projeto_DA/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto_DA.Models;
+
+namespace Projeto_DA
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Multa> Multas { get; set; }
+        public DbSet<Extra> Extras { get; set; }
+        public DbSet<Utilizador> Utilizadores { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Prato> Pratos { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        //public DbSet<Fatura> Faturas { get; set; }
+
+    }
+}
+
+
---
+++ Projeto_DA/Program.cs
@@ -0,0 +1,25 @@
+using Projeto_DA.Vistas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_DA
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormPrincipal());
+
+
+        }
+    }
+}
